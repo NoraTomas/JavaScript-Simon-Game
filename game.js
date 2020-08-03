6a2603e02649723b000728f722226a5c54115c0e
Nora Tomas <noratomas@hotmail.com>
2020-08-03 07:54:29 +0200

    added animation and sound to buttons

--- game.js
+++ game.js
@@ -1,8 +1,29 @@
-$("#green").click(function () {
-  const greenAudio = new Audio("sounds/green.mp3");
-  greenAudio.play();
-  $("#green").addClass("pressed");
+$(".btn").click(function () {
+  console.log(this.id);
+  $(this).addClass("pressed");
   setTimeout(function () {
-    $("#green").removeClass("pressed");
+    $(".btn").removeClass("pressed");
   }, 50);
+
+  switch (this.id) {
+    case "green":
+      const greenAudio = new Audio("sounds/green.mp3");
+      greenAudio.play();
+      break;
+    case "red":
+      const redAudio = new Audio("sounds/red.mp3");
+      redAudio.play();
+      break;
+    case "yellow":
+      const yellowAudio = new Audio("sounds/yellow.mp3");
+      yellowAudio.play();
+      break;
+    case "blue":
+      const blueAudio = new Audio("sounds/blue.mp3");
+      blueAudio.play();
+      break;
+
+    default:
+      break;
+  }
 });
